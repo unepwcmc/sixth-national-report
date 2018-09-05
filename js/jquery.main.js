771d4a0e8cec3cbdac7bf8f5f82b97deddb2a561
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-09-05 16:34:55 +0100

    Redirect to thankyou page after ajax success

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -629,6 +629,7 @@ function initAccordion() {
 						data: this.$form.serialize(),
 						success: function() {
 							self.$form.addClass(self.options.successSendClass);
+              window.location.href = '/thankyou.html'
 						}
 					});
 				}
